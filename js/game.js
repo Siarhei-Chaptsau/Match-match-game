be41f14dd0298677411e1b93017be15cafce273b
Siarhei <s.chaptsau@gmail.com>
2018-06-10 00:33:33 +0300

    добавление таблицы рекордов ТОП-10

--- js/game.js
+++ js/game.js
@@ -2,83 +2,87 @@
 
 const ENTER_KEYCODE = 13;
 const ESC_KEYCODE = 27;
-let rules = document.querySelector('.rules');
-let form = document.querySelector('.form');
-let btn = document.querySelector('.form__btn');
-let formListShirt = document.querySelector('.form__list--shirt');
-let formImage = document.querySelectorAll('.form__image');
-let formListLevel = document.querySelector('.form__list--level');
-let formItemLevel = document.querySelectorAll('.form__text--level');
+const rules = document.querySelector('.rules');
+const form = document.querySelector('.form');
+const btn = document.querySelector('.form__btn');
+const formListShirt = document.querySelector('.form__list--shirt');
+const formImage = document.querySelectorAll('.form__image');
+const formListLevel = document.querySelector('.form__list--level');
+const formItemLevel = document.querySelectorAll('.form__text--level');
 
-let cards = document.querySelector('.cards');
-let cardsBtn = document.querySelector('.cards__btn');
-let cardsItems = document.querySelector('.cards__items');
-let timeInSeconds = document.getElementById('sec');
-let timeInMinutes = document.getElementById('min');
+const cards = document.querySelector('.cards');
+const cardsBtn = document.querySelector('.cards__btn');
+const cardsItems = document.querySelector('.cards__items');
+const timeInSeconds = document.getElementById('sec');
+const timeInMinutes = document.getElementById('min');
 let countTime;
 
-let popupGame = document.querySelector('.popup--game');
-let popupBtnExit = document.querySelector('.popup__btn--exit');
-let failure = document.querySelector('.popup--failure');
-let popupBtnFailure = document.querySelector('.popup__btn--failure');
-let popupTime = document.querySelector('.popup__title--time'); // поле вывода результата игроку
-let popupBtnGame = document.querySelector('.popup__btn--game');
+const popupGame = document.querySelector('.popup--game');
+const popupBtnExit = document.querySelector('.popup__btn--exit');
+const failure = document.querySelector('.popup--failure');
+const popupBtnFailure = document.querySelector('.popup__btn--failure');
+const popupTime = document.querySelector('.popup__title--time'); // поле вывода результата игроку
+const popupBtnGame = document.querySelector('.popup__btn--game');
+const popupTable = document.querySelector('.popup__table');
 
 let firstTurnedCardIndex; // дефолтное значение индекса первой карты
 let firstTurnedCardId; // дефолтное значение data-id первой карты
-let obj = { // объект с инфой игрока
+
+const player = { // объект с инфой игрока
   name: null,
   surname: null,
   email: null,
   score: null
 };
+let ratingList = []; // массив всех результатов
+let ratingItem = []; // массив куда запишем имя и время
+
 let memoryObj = {}; // запоминает какую выбрали рубашку и сложность для игры
 let newArrCardsRandomAndSelected = []; // массив рандомных карт согласно опций
-let arr = [
-{'dataId':1, 'backgroundImage':"Barney_Gumble.jpg"},
-{'dataId':2, 'backgroundImage':"Clancy_Wiggum.jpg"},
-{'dataId':3, 'backgroundImage':"Bart_Simpson.jpg"},
-{'dataId':4, 'backgroundImage':"Eleanor_Abernathy.jpg"},
-{'dataId':5, 'backgroundImage':"Groundskeeper_Willie.jpg"},
-{'dataId':6, 'backgroundImage':"Homer_Simpson.jpg"},
-{'dataId':7, 'backgroundImage':"Horatio_McCallister.jpg"},
-{'dataId':8, 'backgroundImage':"Fat_Tony.jpg"},
-{'dataId':9, 'backgroundImage':"Mr_Burns.jpg"},
-{'dataId':10, 'backgroundImage':"Moe_Szyslak.jpg"},
-{'dataId':11, 'backgroundImage':"Abraham_Simpson.jpg"},
-{'dataId':12, 'backgroundImage':"Seymour_Skinner.jpg"}
+const dataOfCards = [
+  {dataId: 1, backgroundImage: 'Barney_Gumble.jpg'},
+  {dataId: 2, backgroundImage: 'Clancy_Wiggum.jpg'},
+  {dataId: 3, backgroundImage: 'Bart_Simpson.jpg'},
+  {dataId: 4, backgroundImage: 'Eleanor_Abernathy.jpg'},
+  {dataId: 5, backgroundImage: 'Groundskeeper_Willie.jpg'},
+  {dataId: 6, backgroundImage: 'Homer_Simpson.jpg'},
+  {dataId: 7, backgroundImage: 'Horatio_McCallister.jpg'},
+  {dataId: 8, backgroundImage: 'Fat_Tony.jpg'},
+  {dataId: 9, backgroundImage: 'Mr_Burns.jpg'},
+  {dataId: 10, backgroundImage: 'Moe_Szyslak.jpg'},
+  {dataId: 11, backgroundImage: 'Abraham_Simpson.jpg'},
+  {dataId: 12, backgroundImage: 'Seymour_Skinner.jpg'}
 ];
 
 // функция рандомного перемешивания
 Array.prototype.shuffle = function() {
-  let that = this;
-  for (let i = that.length - 1; i >= 0; i--) {
+  for (let i = this.length - 1; i >= 0; i--) {
     let randomIndex = Math.floor(Math.random() * (i + 1));
-    let itemAtIndex = that[randomIndex];
-    that[randomIndex] = that[i];
-    that[i] = itemAtIndex;
+    let itemAtIndex = this[randomIndex];
+    this[randomIndex] = this[i];
+    this[i] = itemAtIndex;
   }
-  return that;
+  return this;
 }
 
 // функция создания рандомного массива согласно выбранного уровня
 function randomMixArrays(start, end) {
-  let arrCut = arr.slice(start, end);
+  let arrCut = dataOfCards.slice(start, end);
   let arrCopy = arrCut.slice();
   newArrCardsRandomAndSelected = arrCut.concat(arrCopy);
   newArrCardsRandomAndSelected.shuffle();
   return newArrCardsRandomAndSelected;
 }
 
-var fragment = document.createDocumentFragment();
-let cardsItem = document.createElement('img');
+const fragment = document.createDocumentFragment();
+const cardsItem = document.createElement('img');
 fragment.appendChild(cardsItem);
 cardsItem.classList.add('cards__item');
 
 // функция начала игры
 function init(obj) {
   // рандомное перемешивание первонач массива
-  arr.shuffle();
+  dataOfCards.shuffle();
   // поменять рубашку карт согласно наличию класса form__active
   for (let i = 0; i < formImage.length; i++) {
     if (formImage[i].classList.contains('form__active')) {
@@ -103,7 +107,7 @@ function init(obj) {
 
 // функция отрисовки каждой карты
 let renderCard = function (card, index) {
-  let newCard = document.createElement('img');
+  const newCard = document.createElement('img');
   newCard.dataset.id = newArrCardsRandomAndSelected[index].dataId; // каждой карте даём id
   newCard.dataset.bg = newArrCardsRandomAndSelected[index].backgroundImage; // сохряняем картинку в атрибут data-bg
   newCard.style.backgroundImage = memoryObj.shirt;
@@ -115,12 +119,12 @@ let renderCard = function (card, index) {
 let addCards = function () {
   for (let i = 0; i < newArrCardsRandomAndSelected.length; i++) {
     let elem = renderCard(newArrCardsRandomAndSelected[i], i);
-    elem.className = "cards__item  cards__item--medium-difficulty";
+    elem.className = 'cards__item  cards__item--medium-difficulty';
     if (newArrCardsRandomAndSelected.length < 18) {
-      elem.className = "cards__item  cards__item--low-difficulty";
+      elem.className = 'cards__item  cards__item--low-difficulty';
     }
     if (newArrCardsRandomAndSelected.length > 18) {
-      elem.className = "cards__item  cards__item--hard-difficulty";
+      elem.className = 'cards__item  cards__item--hard-difficulty';
     }
     cardsItems.appendChild(elem); // добавляем карты на поле
   }
@@ -154,14 +158,74 @@ function outputResult() {
   if (!popupGame.classList.contains('popup--show') ) {
     popupGame.classList.add('popup--show');
   }
-  // заполняем профиль игрока в объект и добавляем в массив
-  obj.name = document.getElementsByName('name')[0].value;
-  obj.surname = document.getElementsByName('surname')[0].value;
-  obj.email = document.getElementsByName('email')[0].value;
-  obj.score = +timeInMinutes.textContent * 60 + +timeInSeconds.textContent; // перевод в секунды
-  let nam = obj.name;
-  let resultsnam = obj.score;
-  localStorage.setItem(nam, resultsnam);
+
+  // заполняем профиль игрока в объект и добавляем в хранилище
+  player.name = document.getElementsByName('name')[0].value;
+  player.surname = document.getElementsByName('surname')[0].value;
+  player.email = document.getElementsByName('email')[0].value;
+  player.score = +timeInMinutes.textContent * 60 + +timeInSeconds.textContent; // перевод в секунды
+  window.localStorage.setItem('player', JSON.stringify(player)); // в хранилище будет добавлено значение
+
+  // добавляем время игрока в массив и сохраняем таблицу 10-ти лучших в хранилище
+  ratingList = JSON.parse(window.localStorage.getItem('ratingList')); // вернёт массив значений лежащих в хранилище
+  if (!ratingList) {
+    ratingList = [];
+  }
+  if (ratingList.length == 10) {
+    ratingList.sort(function(a, b) {
+      return a[1] - b[1];
+    });
+    ratingList = ratingList.slice(0, 9);
+  }
+
+  const nameUser = document.getElementsByName('name')[0].value;
+  let scoreUser = +timeInMinutes.textContent * 60 + +timeInSeconds.textContent; // перевод в секунды
+  ratingItem[0] = nameUser;
+  ratingItem[1] = scoreUser;
+  ratingList.push(ratingItem);
+
+  if ((ratingList.length > 0) && ratingList[ratingList.length - 1][1] > scoreUser) {
+    ratingList[ratingList.length - 1][0] = nameUser;
+    ratingList[ratingList.length - 1][1] = scoreUser;
+  }
+  if (ratingList.length > 1) {
+    ratingList.sort(function(a, b) {
+      return a[1] - b[1];
+    });
+  }
+  window.localStorage.setItem('ratingList', JSON.stringify(ratingList)); // в хранилище будет добавлено значение
+
+  // отрисовываем таблицу результатов
+  const ratingTable = document.createElement('table');
+  const headRow = document.createElement('tr');
+  ratingTable.appendChild(headRow);
+  ratingTable.classList.add('popup__table-tag');
+
+  // отрисовываем шапку
+  for (let i = 0; i < 3; i++) {
+    const headCell = document.createElement('th');
+    headCell.classList.add('popup__cell');
+    if (i == 0) headCell.innerHTML = '№';
+    if (i == 1) headCell.innerHTML = 'Name';
+    if (i == 2) headCell.innerHTML = 'Time';
+    headRow.appendChild(headCell);
+  }
+
+  for (let i = 0; i < 10; i++) {
+    const tableRow = document.createElement('tr');
+    ratingTable.appendChild(tableRow);
+    for (let j = 0; j < 3; j++) {
+      const tableCell = document.createElement('td');
+      if (j == 0) tableCell.innerHTML = `${i + 1}`; // внести номер позиции
+      if (ratingList[i]) {
+        if (j == 1) tableCell.innerHTML = `${ratingList[i][0]}`; // внести имя
+        if (j == 2) tableCell.innerHTML = `${ratingList[i][1]}`; // внести время
+      }
+      tableCell.classList.add('popup__cell');
+      tableRow.appendChild(tableCell);
+    }
+  }
+  popupTable.appendChild(ratingTable);
 }
 
 // обработчик поворота карты
@@ -177,7 +241,7 @@ cardsItems.addEventListener('click', function(e) {
     setTimeout(function() {
       e.target.style.backgroundImage =  "url('./img/" + e.target.getAttribute('data-bg') + "'), url('./img/white.png')";
     }, 300);
-    let arrOfCards = document.querySelectorAll('.cards__item'); // массив карт
+    const arrOfCards = document.querySelectorAll('.cards__item'); // массив карт
     let count = 0; // счётчик для кол-ва открытых карт
 
     for (let i = 0; i < arrOfCards.length; i++) {
@@ -218,7 +282,7 @@ cardsItems.addEventListener('click', function(e) {
             arrOfCards[firstTurnedCardIndex].classList.toggle('cards__item--turned');
             firstTurnedCardId = null; // удаляем id первой карты из глобальной области видимости
             firstTurnedCardIndex = null; // удаляем индекс первой карты из глобальной области видимости
-          }, 900);
+          }, 700);
         }
         break;
       }
@@ -280,9 +344,9 @@ cardsBtn.addEventListener('keydown', enterPressHandler);
 
 // обработчик закрытия попапа
 popupBtnFailure.addEventListener('click', function() {
-if (failure.classList.contains('popup--show')) {
-  failure.classList.remove('popup--show');
-}
+  if (failure.classList.contains('popup--show')) {
+    failure.classList.remove('popup--show');
+  }
 });
 
 // обработчик закрытия попапов ESC
